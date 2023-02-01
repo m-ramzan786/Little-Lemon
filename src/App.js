a611482b7903e092b01a5731efbcf54986e7ea6c
Ramzan <mramzanstv@gmail.com>
2023-02-01 21:28:06 +0500

    Add components and symentic tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import "./App.css";
+import Nav from "./Components/Nav";
+import Header from "./Components/Header";
+import Main from "./Components/Main";
+import Footer from "./Components/Footer";
 
 function App() {
   return (
     <>
-      <h1>hellow world</h1>
+      <Nav />
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
